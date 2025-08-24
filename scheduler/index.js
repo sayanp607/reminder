b521633ccefdaec402f99621b105e96590110d49
Sayan Paul <sayanp607@gmail.com>
2025-08-24 23:26:52 +0530

    changes

--- scheduler/index.js
+++ scheduler/index.js
@@ -20,7 +20,9 @@ const producer = kafka.producer();
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  maxRetriesPerRequest: null 
+  password: process.env.REDIS_PASSWORD,
+  tls: {}, // Upstash requires TLS
+  maxRetriesPerRequest: null
 });
 
 const queue = new Queue('reminder-queue', { connection: redis });
